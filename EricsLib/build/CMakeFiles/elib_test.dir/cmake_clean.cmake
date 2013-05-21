14fa0fabf1acbf4106f7a6021a2892e04556116e
Eric Fiselier <eric@efcs.ca>
2013-05-20 18:23:07 -0600

    test

---
+++ EricsLib/build/CMakeFiles/elib_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/elib_test.dir/tests/lock_test.cpp.o"
+  "../elib_test.pdb"
+  "../elib_test"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/elib_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)

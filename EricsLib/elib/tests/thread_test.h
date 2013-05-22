544d331ea6aeb685465fef4e1cc56d171ab96e71
Eric Fiselier <eric@efcs.ca>
2013-05-22 21:41:40 +0000

    thread_test.h edited online with Bitbucket

--- EricsLib/elib/tests/thread_test.h
+++ EricsLib/elib/tests/thread_test.h
@@ -19,17 +19,15 @@ void run_thread_tests(std::vector<FirstT> first,
 			   std::vector<RestT>... rest)
 {
 	std::vector<std::thread*> threads;
-	for (auto it=first.begin(); it != first.end(); ++it) {
-		threads.push_back( new std::thread(&RunInterface::run, (*it)) );
-	}
+	
+	for (auto &item : first) 
+		threads.push_back( new std::thread(&RunInterface::run, item) );
 	
 	run_thread_tests(rest...);
 	
-	for (auto it=threads.begin(); it != threads.end(); ++it) {
-		(*it)->join();
-	}
+	for (auto &item : threads) 
+		item->join();
 	
-	return;
 }
 
 #endif /* TESTING_THREAD_TEST_H */

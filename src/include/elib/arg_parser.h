8c2c7ae3d54ff336f4f113e6142cd149cb1e38c0
Eric Fiselier <eric@efcs.ca>
2013-06-14 06:41:39 -0600

    clean up

--- src/include/elib/arg_parser.h
+++ src/include/elib/arg_parser.h
@@ -3,15 +3,9 @@
 
 #include <string>
 #include <memory>
+#include <vector>
 
 namespace elib {
-
-
-struct added_order_t { };
-struct argv_order_t { };
-constexpr added_order_t in_order_added = added_order_t();
-constexpr argv_order_t in_order_argv = argv_order_t;
-
 
 
 class argp {
@@ -35,8 +29,8 @@ private:
 	
 	
 public:
-	argp();
-	~argp();
+	argp() { }
+	~argp() { }
 	
 	
 	template <typename T, typename Func, typename... Args>
@@ -56,75 +50,19 @@ public:
 	}
 	
 	
-	
-	
 	void process_args(int argc, const char *argv[], 
-					  std::vector<std::string>& unused,
-					  const argv_order_t &t) {
-		UNUSED(t);
-		for (int i=0; i < argc; ++i) {
-			auto it = std::find(m_arg_list.begin(), m_arg_list.end(), argv[i]);
-			if (it == m_arg_list.end()) {
-				unused.push_back(std::string(argv[i]));
-			} else {
-				(*it)->set_func();
-				(*it)->seen = true;
-			}
-		}
-		for (auto &arg : m_arg_list) {
-			if (! arg->seen ) {
-				if (arg->optional) {
-					arg->seen_func();
-					arg->seen = true;
-				} else {
-					//ERROR
-					//TODO
-				}
-			}
-		}
-	}
-	
-	void process_args(int argc, const char *argv[], 
-					  std::vector<std::string>& unused,
-					  const added_order_t &t) {
-		UNUSED(t);
+					  std::vector<std::string>& unused) {
 		for (int i=0; i < argc; ++i)
 			unused.push_back(std::string(argv[i]));
 		for (auto &arg : m_arg_list) {
 			std::remove(unused.begin(), unused.end(), arg->cmd_line_flag);
 			arg->set_func();
-			arg->seen = true;
 		}
-	}
-	
-private:
-	bool _process_arg(const char *arg) {
-
-		auto it = std::find_if(m_arg_list.begin(), m_arg_list.end(),
-								[&](std::shared_ptr<__arg> m_arg) 
-											{ (*m_arg) == arg; } );
-		
-		if (it != m_arg_list.end()) {
-			it->set_func();
-			it->seen = true;
-			return true;
-		}
-		
-		return false;
 	}
 	
 private:
 	std::vector<std::shared_ptr<__arg>> m_arg_list;
-	_elib::arg_parser 
 };
-
-
-
-
-
-
-
-
 
 
 } /* namespace elib */

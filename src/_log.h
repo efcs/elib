6ac5d4e444515f9d5d34a60fce3867f5ad1a8fae
Eric Fiselier <eric@efcs.ca>
2013-06-27 16:29:23 -0600

    auto commit

--- src/_log.h
+++ src/_log.h
@@ -1,3 +1,21 @@
+/* 
+ * Copyright (C) 2013  Eric Fiselier
+ * 
+ * This file is part of elib.
+ *
+ * elib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * elib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with elib.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef _LOG_H
 #define _LOG_H
 

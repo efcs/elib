abfe4fab17edda5c0f8ee213add5b6b4509521b3
Eric Fiselier <eric@efcs.ca>
2013-11-23 03:25:11 -0700

    changed bitwise operator iterface to require at least 2 params

--- elib/mp/bitwise.hpp
+++ elib/mp/bitwise.hpp
@@ -10,6 +10,10 @@ namespace elib
   namespace mp
   {
     
+  ////////////////////////////////////////////////////////////////////////////////
+  //                            BITNEGATE
+  ////////////////////////////////////////////////////////////////////////////////
+    
     template <class T>
     struct bitnegate_ 
       : std::integral_constant<
@@ -22,55 +26,74 @@ namespace elib
   //                       BITAND                                                   
   ////////////////////////////////////////////////////////////////////////////////
     
-    template <class ...Args>
-    struct bitand_;
+    namespace detail 
+    {
+      
+      template <class ...Args>
+      struct bitand_impl;
+      
+      template <class T>
+      struct bitand_impl<T> : T
+      {};
+      
+      template <class A1, class A2>
+      struct bitand_impl<A1, A2> 
+        : std::integral_constant<
+            decltype( A1::value & A2::value)
+            , A1::value & A2::value
+          >
+      {};
+      
+      template <class A1, class A2, class A3, class ...Args>
+      struct bitand_impl<A1, A2, A3, Args...>
+        : bitand_impl<
+            bitand_impl<A1, A2>
+            , bitand_impl<A3, Args...>
+          >
+      {};
+      
+    }                                                       // namespace detail
     
-    template <class T>
-    struct bitand_<T> : T
+    template <class A1, class A2, class ...Rest>
+    struct bitand_ : detail::bitand_impl<A1, A2, Rest...>
     {};
     
-    template <class A1, class A2>
-    struct bitand_<A1, A2> 
-      : std::integral_constant<
-          decltype( A1::value & A2::value)
-          , A1::value & A2::value
-        >
-    {};
-    
-    template <class A1, class A2, class A3, class ...Args>
-    struct bitand_<A1, A2, A3, Args...>
-      : bitand_<
-          bitand_<A1, A2>
-          , bitand_<A3, Args...>
-        >
-    {};
     
   ////////////////////////////////////////////////////////////////////////////////
   //                           BITOR                                               
   ////////////////////////////////////////////////////////////////////////////////
     
+    namespace detail
+    {
+        
+      template <class ...Args>
+      struct bitor_impl;
+      
+      template <class T>
+      struct bitor_impl<T> : T
+      {};
+      
+      template <class A1, class A2>
+      struct bitor_impl<A1, A2> 
+        : std::integral_constant<
+            decltype( A1::value | A2::value)
+            , A1::value | A2::value
+          >
+      {};
+      
+      template <class A1, class A2, class A3, class ...Args>
+      struct bitor_impl<A1, A2, A3, Args...>
+        : bitor_impl<
+            bitor_impl<A1, A2>
+            , bitor_impl<A3, Args...>
+          >
+      {};
+      
+    }                                                       // namespace detail
     
-    template <class ...Args>
-    struct bitor_;
     
-    template <class T>
-    struct bitor_<T> : T
-    {};
-    
-    template <class A1, class A2>
-    struct bitor_<A1, A2> 
-      : std::integral_constant<
-          decltype( A1::value | A2::value)
-          , A1::value | A2::value
-        >
-    {};
-    
-    template <class A1, class A2, class A3, class ...Args>
-    struct bitor_<A1, A2, A3, Args...>
-      : bitor_<
-          bitor_<A1, A2>
-          , bitor_<A3, Args...>
-        >
+    template <class A1, class A2, class ...Rest>
+    struct bitor_ : detail::bitor_impl<A1, A2, Rest...>
     {};
     
     
@@ -78,84 +101,61 @@ namespace elib
   //                               BITXOR                                           
   ////////////////////////////////////////////////////////////////////////////////
     
+    namespace detail
+    {
+      
+      template <class ...Args>
+      struct bitxor_impl;
+      
+      template <class T>
+      struct bitxor_impl<T> : T
+      {};
+      
+      template <class A1, class A2>
+      struct bitxor_impl<A1, A2> 
+        : std::integral_constant<
+            decltype( A1::value ^ A2::value)
+            , A1::value ^ A2::value
+          >
+      {};
+      
+      template <class A1, class A2, class A3, class ...Args>
+      struct bitxor_impl<A1, A2, A3, Args...>
+        : bitxor_impl<
+            bitxor_impl<A1, A2>
+            , bitxor_impl<A3, Args...>
+          >
+      {};
+      
+    }                                                       // namespace detail
     
-    template <class ...Args>
-    struct bitxor_;
-    
-    template <class T>
-    struct bitxor_<T> : T
+    template <class A1, class A2, class ...Rest>
+    struct bitxor_ : detail::bitxor_impl<A1, A2, Rest...>
     {};
-    
-    template <class A1, class A2>
-    struct bitxor_<A1, A2> 
-      : std::integral_constant<
-          decltype( A1::value ^ A2::value)
-          , A1::value ^ A2::value
-        >
-    {};
-    
-    template <class A1, class A2, class A3, class ...Args>
-    struct bitxor_<A1, A2, A3, Args...>
-      : bitxor_<
-          bitxor_<A1, A2>
-          , bitxor_<A3, Args...>
-        >
-    {};
-    
     
   ////////////////////////////////////////////////////////////////////////////////
   //                          SHIFT LEFT                                                
   ////////////////////////////////////////////////////////////////////////////////
   
   
-    template <class ...Args>
-    struct shift_left;
-    
-    template <class T>
-    struct shift_left<T> : T
-    {};
-    
-    template <class A1, class A2>
-    struct shift_left<A1, A2> 
-      : std::integral_constant<
-          decltype( A1::value << A2::value)
-          , (A1::value << A2::value)
-        >
-    {};
-    
-    template <class A1, class A2, class A3, class ...Args>
-    struct shift_left<A1, A2, A3, Args...>
-      : shift_left<
-          shift_left<A1, A2>
-          , shift_left<A3, Args...>
-        >
-    {};
+   template <class C, class Shift>
+   struct shift_left 
+     : std::integral_constant<
+        decltype(C::value << Shift::value)
+        , (C::value << Shift::value)
+      >
+  {};
     
   ////////////////////////////////////////////////////////////////////////////////
   //                         SHIFT RIGHT                                                 
   ////////////////////////////////////////////////////////////////////////////////
   
     
-    template <class ...Args>
-    struct shift_right;
-    
-    template <class T>
-    struct shift_right<T> : T
-    {};
-    
-    template <class A1, class A2>
-    struct shift_right<A1, A2> 
+    template <class C, class Shift>
+    struct shift_right 
       : std::integral_constant<
-          decltype( A1::value >> A2::value)
-          , (A1::value >> A2::value)
-        >
-    {};
-    
-    template <class A1, class A2, class A3, class ...Args>
-    struct shift_right<A1, A2, A3, Args...>
-      : shift_right<
-          shift_right<A1, A2>
-          , shift_right<A3, Args...>
+          decltype(C::value >> Shift::value)
+          , (C::value >> Shift::value) 
         >
     {};
   

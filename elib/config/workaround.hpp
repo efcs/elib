def1fb487bcdfa8a20bc8a33f71dd9189b37dd37
Eric Fiselier <eric@efcs.ca>
2014-01-07 03:36:22 -0700

    clean up

--- elib/config/workaround.hpp
+++ elib/config/workaround.hpp
@@ -6,7 +6,7 @@
 # 
 # /* A clang bug */
 # if defined(__clang__)
-#   if __clang_major__ <= 3 && __clang_minor__ <= 4
+#   if __clang_major__ <= 3 && __clang_minor__ < 4
 #     define ELIB_WORKAROUND_CLANG_ENUMERATION_COMPARE_BUG 1
 #   endif
 # endif                                                      // clang bugs

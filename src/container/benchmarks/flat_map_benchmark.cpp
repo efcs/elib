a09aa25bedd5ac96575e5c5ce295eb16a2df1169
Eric Fiselier <eric@efcs.ca>
2014-04-08 01:41:25 -0600

    validating

--- src/container/benchmarks/flat_map_benchmark.cpp
+++ src/container/benchmarks/flat_map_benchmark.cpp
@@ -12,18 +12,20 @@
 #define STR(x) XSTR(x)
 #define XSTR(x) #x
 
-using value_type = std::pair<int, int>;
-using flat_map = elib::flat_map<int, int>;
-using linear_flat_map = elib::linear_flat_map<int, int>;
-using standard_map = std::map<int, int>;
+using int_type = int;
+
+using value_type = std::pair<int_type, int_type>;
+using flat_map = elib::flat_map<int_type, int_type>;
+using linear_flat_map = elib::linear_flat_map<int_type, int_type>;
+using standard_map = std::map<int_type, int_type>;
 
 
-std::vector<value_type> make_values(int to_add)
+std::vector<value_type> make_values(int_type to_add)
 {
     std::vector<value_type> dest;
     dest.reserve(to_add);
     
-    for (int i=0; i < to_add; ++i) {
+    for (int_type i=0; i < to_add; ++i) {
         dest.emplace_back(value_type(i, i));
     }
     
@@ -41,7 +43,7 @@ MapType construct_test(std::vector<value_type> const & v)
 template <class MapType>
 void find_test(std::vector<value_type> const & v, MapType const & m)
 {
-    for (int i=0; i < 10; ++i) {
+    for (int_type i=0; i < 10; ++i) {
         for (auto & kv : v) {
             m.find(kv.first);
         }
@@ -49,10 +51,10 @@ void find_test(std::vector<value_type> const & v, MapType const & m)
 }
 
 template <class MapType>
-int iter_test(MapType const & m)
+int_type iter_test(MapType const & m)
 {
-    int accum = 0;
-    for (int i=0; i < 100; ++i) {
+    int_type accum = 0;
+    for (int_type i=0; i < 100; ++i) {
         for (auto & kv : m) {
             accum += kv.first;
             accum %= 2;
@@ -85,9 +87,9 @@ int main()
 {
     std::cout << "Testing " STR(MAP_TYPE) << std::endl;
     using Map = MAP_TYPE;
-    auto kv = make_values(100000000);
+    auto kv = make_values(10000000);
     Map m;
-    int x = 0;
+    int_type x = 0;
     time("Construct", [&]() { m = construct_test<Map>(kv); });
     //time("Find", [&]() { find_test(kv, m); });
     time("Iterate", [&]() { x = iter_test(m); });

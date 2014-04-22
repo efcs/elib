be9827736e92bc0b6895274484405fa7bb76e3f7
Eric Fiselier <eric@efcs.ca>
2014-04-21 21:05:26 -0600

    removed uneeded posix

--- elib/posix/fwd.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef ELIB_POSIX_FWD_HPP
-#define ELIB_POSIX_FWD_HPP
-
-namespace elib { namespace posix
-{
-    
-}}                                                          // namespace elib
-#endif /* ELIB_POSIX_FWD_HPP */
--- src/posix/build.cmake
+++
@@ -1,8 +0,0 @@
-
-add_src()
-add_definitions( 
-    -DELIB_FS_UNIT_TEST_PATH="${CMAKE_CURRENT_LIST_DIR}/unit_tests"
-)
-add_definitions(
-    -DELIB_FS_ELIB_ROOT_PATH="${CONFIG_ELIB_ROOT}"
-)

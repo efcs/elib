b54c45163f054724384fe8cae723c00e34087c4a
Eric Fiselier <eric@efcs.ca>
2014-04-25 17:25:50 -0600

    fixed default param for aligned_storage_t

--- elib/aux/traits/aligned_storage.hpp
+++ elib/aux/traits/aligned_storage.hpp
@@ -1,6 +1,7 @@
 #ifndef ELIB_AUX_TRAITS_ALIGNED_STORAGE_HPP
 #define ELIB_AUX_TRAITS_ALIGNED_STORAGE_HPP
 
+# include <elib/aux/integral_constant.hpp>
 # include <type_traits>
 # include <cstddef>
 
@@ -10,11 +11,25 @@ namespace elib { namespace aux
     {
         using std::aligned_storage;
         
+        namespace traits_detail
+        {
+            template <class T>
+            struct deduce_align_impl;
+            
+            template <std::size_t Len, std::size_t Align>
+            struct deduce_align_impl<aligned_storage<Len, Align>>
+              : elib::integral_constant<std::size_t, Align>
+            {};
+            
+            template <std::size_t Len>
+            using deduced_align = deduce_align_impl<aligned_storage<Len>>;
+        }                                                   // namespace traits_detail
+        
         template <
             std::size_t Len
-          , std::size_t Align = static_cast<std::size_t>(-1)
+          , std::size_t Align = traits_detail::deduced_align<Len>::value
           >
-        using aligned_storage_t = typename aligned_storage<Len, Align>::type;
+        using aligned_storage_t = typename aligned_storage<Len, Align>::value;
     }                                                       //namespace traits
     
     using namespace traits;

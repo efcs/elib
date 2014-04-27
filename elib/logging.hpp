a710283d19ff7664591c58f9f28cbc6483461cc7
Eric Fiselier <eric@efcs.ca>
2014-04-26 21:01:35 -0600

    removed reference to deleted header

--- elib/logging.hpp
+++ elib/logging.hpp
@@ -1,7 +1,6 @@
 #ifndef ELIB_LOGGING_HPP
 #define ELIB_LOGGING_HPP
 # 
-# include <elib/logging/config.hpp>
 # include <elib/logging/basic_log.hpp>
 # include <elib/logging/elog.hpp>
 # include <elib/logging/file_log.hpp>

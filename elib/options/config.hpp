3f9943ea1c59ffd43bcfc8315125bf4288ff600a
Eric Fiselier <eric@efcs.ca>
2014-04-27 19:35:39 -0600

    remove old options library

--- elib/options/config.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef ELIB_OPTIONS_CONFIG_HPP
-#define ELIB_OPTIONS_CONFIG_HPP
-
-namespace elib { namespace options
-{
-    constexpr unsigned value_semantics_max_tokens = 15;
-}}                                                          // namespace elib
-#endif /* ELIB_OPTIONS_CONFIG_HPP */

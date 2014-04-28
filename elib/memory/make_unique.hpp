42a8968f96307d9150855c6b50e248bf46a72a82
Eric Fiselier <eric@efcs.ca>
2014-04-27 19:25:05 -0600

    update make_unique to be inline with new proposal

--- elib/memory/make_unique.hpp
+++ elib/memory/make_unique.hpp
@@ -1,6 +1,7 @@
 #ifndef ELIB_MEMORY_MAKE_UNIQUE_HPP
 #define ELIB_MEMORY_MAKE_UNIQUE_HPP
 
+# include <elib/aux/always.hpp>
 # include <elib/aux/forward.hpp>
 # include <memory>
 # include <cstddef>
@@ -24,10 +25,14 @@ namespace elib { namespace memory
         template <class T, std::size_t N>
         struct unique_if<T[N]>
         {
-            using known_bound = void;
+            static_assert(
+                aux::never<T>::value
+              , "make_unique forbids T[N]. please use T[]"
+              );
         };
     }                                                       // namespace detail
     
+    ////////////////////////////////////////////////////////////////////////////
     template <class T, class... Args>
     typename detail::unique_if<T>::single_object
     make_unique(Args&&... args) 
@@ -35,6 +40,15 @@ namespace elib { namespace memory
         return std::unique_ptr<T>(new T(elib::forward<Args>(args)...));
     }
     
+    ////////////////////////////////////////////////////////////////////////////
+    template <class T>
+    typename detail::unique_if<T>::single_object
+    make_unique_default_init() 
+    {
+        return std::unique_ptr<T>(new T);
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////
     template <class T>
     typename detail::unique_if<T>::unknown_bound
     make_unique(size_t n)
@@ -43,6 +57,36 @@ namespace elib { namespace memory
         return std::unique_ptr<T>(new U[n]());
     }
     
+    ////////////////////////////////////////////////////////////////////////////
+    template <class T>
+    typename detail::unique_if<T>::unknown_bound
+    make_unique_default_init(size_t n)
+    {
+        using U = typename aux::remove_extent<T>::type;
+        return std::unique_ptr<T>(new U[n]);
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////
+    template <class T, class ...Args>
+    typename detail::unique_if<T>::unknown_bound
+    make_unique_value_init(size_t n, Args &&... args)
+    {
+        using U = typename aux::remove_extent<T>::type;
+        return std::unique_ptr<T>(new U[n]{std::forward<Args>(args)...});
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////
+    template <class T, class ...Args>
+    typename detail::unique_if<T>::unknown_bound
+    make_unique_auto_size(Args &&... args)
+    {
+        using U = typename aux::remove_extent<T>::type;
+        return std::unique_ptr<T>(
+            new U[sizeof...(Args)]{ elib::forward<Args>(args)...}
+          );
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////
     template <class T, class... Args>
     typename detail::unique_if<T>::known_bound
     make_unique(Args&&...) = delete;  
@@ -50,5 +94,8 @@ namespace elib { namespace memory
 namespace elib
 {
     using memory::make_unique;
+    using memory::make_unique_default_init;
+    using memory::make_unique_value_init;
+    using memory::make_unique_auto_size;
 }                                                           // namespace elib
 #endif /* ELIB_MEMORY_MAKE_UNIQUE_HPP */

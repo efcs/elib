66fc9f417cbcd58a1ed82b3a589c25452f1b9cd5
Eric Fiselier <eric@efcs.ca>
2014-05-04 01:22:44 -0600

    fix build warnings for web

--- elib/web/http/fwd.hpp
+++ elib/web/http/fwd.hpp
@@ -32,57 +32,6 @@ namespace elib { namespace web { namespace http
     
     using request = message<request_header>;
     using response = message<response_header>;
-    
-    data_type serialize(request_header const &);
-    data_type serialize(response_header const &);
-    data_type serialize(field_type const &);
-    data_type serialize(std::vector<field_type> const &);
-    
-    template <class HeaderType>
-    data_type serialize(message<HeaderType> const & m);
-    
-    data_type::const_iterator 
-    parse(
-        data_type::const_iterator begin
-      , data_type::const_iterator const & end
-      , request_header & dest
-    );
-    
-    data_type::const_iterator 
-    parse(
-        data_type::const_iterator begin
-      , data_type::const_iterator const & end
-      , response_header & dest
-    );
-    
-    data_type::const_iterator 
-    parse(
-        data_type::const_iterator begin
-      , data_type::const_iterator const & end
-      , field_type & dest
-    );
-    
-    data_type::const_iterator 
-    parse(
-        data_type::const_iterator begin
-      , data_type::const_iterator const & end
-      , std::vector<field_type> & dest
-    );
-    
-    data_type::const_iterator 
-    parse_newl(
-        data_type::const_iterator begin
-      , data_type::const_iterator const & end
-    );
-    
-    template <class Header>
-    data_type::const_iterator
-    parse(
-        data_type::const_iterator begin
-      , data_type::const_iterator const & end
-      , message<Header> & dest
-    );
-    
-    
+        
 }}}                                                         // namespace elib
 #endif /* ELIB_WEB_HTTP_FWD_HPP */

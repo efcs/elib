8f234cc949b3e06bbcd45377cccabf1244531689
Eric Fiselier <eric@efcs.ca>
2014-05-07 22:43:40 -0600

    create top level file for mp

--- elib/mp.hpp
+++ elib/mp.hpp
@@ -1,12 +1,10 @@
-#ifndef ELIB_MP_METAPROGRAMMING_HPP
-#define ELIB_MP_METAPROGRAMMING_HPP
+#ifndef ELIB_MP_HPP
+#define ELIB_MP_HPP
 # 
 # include <elib/mp/config.hpp>
-# 
 # include <elib/mp/algorithms.hpp>
 # include <elib/mp/containers.hpp>
 # include <elib/mp/get_impl.hpp>
-# include <elib/aux.hpp>
 # include <elib/mp/iterator.hpp>
 # include <elib/mp/member_template_detector.hpp>
 # include <elib/mp/member_type_detector.hpp>
@@ -16,4 +14,4 @@
 # include <elib/mp/variadic.hpp>
 # include <elib/mp/void.hpp>
 # 
-#endif /* ELIB_MP_METAPROGRAMMING_HPP */
+#endif /* ELIB_MP_HPP */

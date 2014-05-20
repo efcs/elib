511f4e151c91648226e162221796629833777fb5
Eric Fiselier <eric@efcs.ca>
2014-05-20 02:44:37 -0600

    fixed value semantics with converter class

--- elib/aux/convert.hpp
+++ elib/aux/convert.hpp
@@ -5,13 +5,20 @@
 # include <elib/aux/forward.hpp>
 # include <elib/aux/move.hpp>
 # include <elib/aux/enable_if.hpp>
-# include <elib/aux/traits/remove_reference.hpp>
+# include <elib/aux/traits/decay.hpp>
+# include <elib/aux/traits/is_reference.hpp>
+
 
 namespace elib { namespace aux
 {
     template <class T>
     struct converter
     {
+        static_assert(
+            not aux::is_reference<T>::value
+          , "T cannot be a reference"
+          );
+        
         explicit constexpr converter(T v)
           : m_value(elib::move(v))
         {}
@@ -19,12 +26,12 @@ namespace elib { namespace aux
         template <
             class U 
           , ELIB_ENABLE_IF_VALID_EXPR(
-                static_cast<U>( elib::forward<T>(elib::declval<T>()) )
+                static_cast<U>( elib::move(elib::declval<T>()) )
             )
           >
         operator U() 
         {
-            return static_cast<U>(elib::forward<T>(m_value));
+            return static_cast<U>(elib::move(m_value));
         }
         
     private:
@@ -32,10 +39,10 @@ namespace elib { namespace aux
     };
     
     template <class T>
-    converter<aux::remove_reference_t<T>>
+    converter<aux::decay_t<T>>
     convert(T && v)
     {
-        return converter<aux::remove_reference_t<T>>(elib::forward<T>(v));
+        return converter<aux::decay_t<T>>(elib::forward<T>(v));
     }
 }}                                                          // namespace elib
 namespace elib

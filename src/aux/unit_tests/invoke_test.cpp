e39b022975771080b81e577d2a708e96b82f54d8
Eric Fiselier <eric@efcs.ca>
2014-05-23 21:51:12 -0600

    improve invoke tests

--- src/aux/unit_tests/invoke_test.cpp
+++ src/aux/unit_tests/invoke_test.cpp
@@ -8,19 +8,46 @@ using namespace elib::aux;
 
 struct class_type
 {
-    int foo(int x) { return x; }
+    class_type() {}
+    
+    int first(int x, int) { return x; }
+    int second(int, int y) const { return y; }
 };
-
 
 
 BOOST_AUTO_TEST_SUITE(elib_aux_invoke_test_suite)
 
-BOOST_AUTO_TEST_CASE(member_function_test)
+BOOST_AUTO_TEST_CASE(non_const_member_function_test)
 {
     class_type c;
-    auto fn_ptr = &class_type::foo;
-    auto ret = invoke(fn_ptr, c, 5);
-    BOOST_CHECK(ret == 5);
+    auto fn_ptr = &class_type::first;
+    auto ret = invoke(fn_ptr, c, 2, 3);
+    BOOST_CHECK(ret == 2);
 }
+
+BOOST_AUTO_TEST_CASE(const_member_function_test)
+{
+    const class_type c;
+    auto fn_ptr = &class_type::second;
+    auto ret = invoke(fn_ptr, c, 2, 3);
+    BOOST_CHECK(ret == 3);
+}
+
+BOOST_AUTO_TEST_CASE(non_const_member_function_from_pointer_test)
+{
+    class_type c;
+    auto fn_ptr = &class_type::first;
+    auto ret = invoke(fn_ptr, &c, 2, 3);
+    BOOST_CHECK(ret == 2);
+}
+
+BOOST_AUTO_TEST_CASE(const_member_function_from_pointer_test)
+{
+    const class_type c;
+    auto fn_ptr = &class_type::second;
+    auto ret = invoke(fn_ptr, &c, 2, 3);
+    BOOST_CHECK(ret == 3);
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()

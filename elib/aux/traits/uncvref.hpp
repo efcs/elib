7d75b76ef30a7acf506d2b62d44f19cefdc6fc3f
Eric Fiselier <eric@efcs.ca>
2014-05-24 15:17:09 -0600

    add tests for uncvref

--- elib/aux/traits/uncvref.hpp
+++ elib/aux/traits/uncvref.hpp
@@ -8,48 +8,9 @@ namespace elib { namespace aux
 {
     template <class T>
     using uncvref = remove_cv_t<remove_reference_t<T>>;
-        
-    namespace detail
-    {
-        template <class T>
-        struct uncvref_all_impl
-        {
-            using type = T;
-        };
-            
-        template <class T>
-        struct uncvref_all_impl<T &> : uncvref_all_impl<T>
-        {
-        };
-            
-        template <class T>
-        struct uncvref_all_impl<T &&> : uncvref_all_impl<T>
-        {
-        };
-            
-        template <class T>
-        struct uncvref_all_impl<T const> : uncvref_all_impl<T>
-        {
-        };
-            
-        template <class T>
-        struct uncvref_all_impl<T volatile> : uncvref_all_impl<T>
-        {
-        };
-            
-        template <class T>
-        struct uncvref_all_impl<T const volatile> : uncvref_all_impl<T>
-        {
-        };
-    }                                                       // namespace detail
-        
-    template <class T>
-    using uncvref_all = typename detail::uncvref_all_impl<T>::type;
-
 }}                                                           //namespace elib
 namespace elib
 {
     using aux::uncvref;
-    using aux::uncvref_all;
 }
 #endif /* ELIB_AUX_TRAITS_UNCVREF_HPP */
---
+++ src/aux/traits_unit_tests/uncvref_test.cpp
@@ -0,0 +1,29 @@
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include <elib/aux/traits/uncvref.hpp>
+#include <elib/aux/traits/is_same.hpp>
+#include <elib/aux/static_assert.hpp>
+using namespace elib::aux;
+
+BOOST_AUTO_TEST_SUITE(elib_aux_traits_uncvref_test_suite)
+
+BOOST_AUTO_TEST_CASE(uncvref_test)
+{
+    ELIB_STATIC_ASSERT(is_same<uncvref<void>, void>::value);
+    ELIB_STATIC_ASSERT(is_same<uncvref<void*>, void*>::value);
+    ELIB_STATIC_ASSERT(is_same<uncvref<const int>, int>::value);
+    ELIB_STATIC_ASSERT(is_same<uncvref<volatile int>, int>::value);
+    ELIB_STATIC_ASSERT(is_same<uncvref<const volatile int>, int>::value);
+    ELIB_STATIC_ASSERT(is_same<uncvref<int &>, int>::value);
+    ELIB_STATIC_ASSERT(is_same<uncvref<int &&>, int>::value);
+    ELIB_STATIC_ASSERT(is_same<uncvref<int const &>, int>::value);
+    ELIB_STATIC_ASSERT(is_same<uncvref<int volatile &>, int>::value);
+    ELIB_STATIC_ASSERT(is_same<uncvref<int const volatile &>, int>::value);
+    ELIB_STATIC_ASSERT(is_same<uncvref<int const &&>, int>::value);
+    ELIB_STATIC_ASSERT(is_same<uncvref<int volatile &&>, int>::value);
+    ELIB_STATIC_ASSERT(is_same<uncvref<int const volatile &&>, int>::value);
+    BOOST_CHECK(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()

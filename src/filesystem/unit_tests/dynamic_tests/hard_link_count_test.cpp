3ffeaee6825ba9333ac29ad6a2fc1a87f6e3b9dd
Eric Fiselier <eric@efcs.ca>
2014-05-26 21:26:20 -0600

    more tests

---
+++ src/filesystem/unit_tests/dynamic_tests/hard_link_count_test.cpp
@@ -0,0 +1,49 @@
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include <elib/filesystem.hpp>
+#include <system_error>
+#include <cstdint>
+#include "../dynamic_test_helper.hpp"
+using namespace elib::fs;
+
+constexpr const std::uintmax_t bad_count = static_cast<std::uintmax_t>(-1);
+
+BOOST_AUTO_TEST_SUITE(elib_filesystem_dynamic_hard_link_count_test_suite)
+
+BOOST_AUTO_TEST_CASE(dne_test)
+{
+    scoped_test_env env;
+    path const file = make_env_path("dne");
+    
+    {
+        std::error_code ec;
+        std::uintmax_t count = hard_link_count(file, ec);
+        BOOST_REQUIRE(ec);
+        BOOST_CHECK(count == bad_count);
+    }
+    {
+        BOOST_REQUIRE_THROW(hard_link_count(file), filesystem_error);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(basic_test)
+{
+    scoped_test_env env;
+    path const file = make_env_path("file1");
+    python_create_file(file);
+    
+    std::error_code ec;
+    std::uintmax_t count = hard_link_count(file, ec);
+    BOOST_REQUIRE(not ec);
+    BOOST_REQUIRE(count == 1);
+    
+    path const link = make_env_path("link1");
+    python_create_hardlink(file, link);
+    
+    count = hard_link_count(file, ec);
+    BOOST_REQUIRE(not ec);
+    BOOST_CHECK(count == 2);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ src/filesystem/unit_tests/static_tests/space_test.cpp
@@ -0,0 +1,43 @@
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include <elib/filesystem.hpp>
+#include <system_error>
+#include <cstdint>
+#include "../static_test_helper.hpp"
+using namespace elib::fs;
+
+constexpr const std::uintmax_t bad_size = static_cast<std::uintmax_t>(-1);
+
+BOOST_AUTO_TEST_SUITE(elib_filesystem_static_space_test_suite)
+
+BOOST_AUTO_TEST_CASE(dne_test)
+{
+    const path file = make_static_env_path("dne");
+    
+    {
+        std::error_code ec;
+        space_info info = space(file, ec);
+        BOOST_REQUIRE(ec);
+        BOOST_CHECK(info.capacity == bad_size);
+        BOOST_CHECK(info.free == bad_size);
+        BOOST_CHECK(info.available == bad_size);
+    }
+    {
+        BOOST_REQUIRE_THROW(space(file), filesystem_error);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(file_test)
+{
+    const path file = make_static_env_path("empty_file");
+    
+    std::error_code ec;
+    space_info info = space(file, ec);
+    BOOST_REQUIRE(not ec);
+    BOOST_CHECK(info.capacity > 0);
+    BOOST_CHECK(info.free > 0 && info.free < info.capacity);
+    BOOST_CHECK(info.available > 0 && info.available < info.capacity);
+}
+
+BOOST_AUTO_TEST_SUITE_END()

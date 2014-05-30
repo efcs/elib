e9c73835c490bc99371c3426028569f501227c6c
Eric Fiselier <eric@efcs.ca>
2014-05-29 22:01:54 -0600

    trying

--- src/filesystem/unit_tests/static_test_helper.hpp
+++ src/filesystem/unit_tests/static_test_helper.hpp
@@ -8,7 +8,7 @@ const elib::fs::path static_test_env_path = ELIB_FILESYSTEM_UNIT_TEST_PATH / eli
 inline elib::fs::path make_static_env_path(elib::fs::path const & p)
 {
     static const elib::fs::path root_p = ELIB_FILESYSTEM_UNIT_TEST_PATH / elib::fs::path("static_test_env");
-    return static_test_env_path / root_p;
+    return root_p / p;
 }
 
 #endif /* STATIC_TEST_HELPER_HPP */

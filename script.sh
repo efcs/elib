87957e846039b5cdb00edaae79ff003ffff470b2
Eric Fiselier <eric@efcs.ca>
2014-06-05 02:08:51 -0600

    clang try 3

--- script.sh
+++ script.sh
@@ -3,6 +3,11 @@ then
     export CXX="g++-4.8"
     export CC="gcc-4.8"
 fi
+if [[ "$CXX" == "clang++" ]]
+then
+    export CXX="clang++-3.5"
+    export CC="clang-3.5"
+fi
 
 make configure
 make -j2

999e3971957ba63b471f81feaa8229ee1df75dad
Eric Fiselier <eric@efcs.ca>
2014-06-05 03:00:21 -0600

    suppress another clang warning

--- travis-script.sh
+++ travis-script.sh
@@ -8,7 +8,7 @@ then
     sudo apt-get install -qq clang-3.5
     export CXX="clang++-3.5"
     export CC="clang-3.5"
-    export CXXFLAGS="$CXXFLAGS -Wno-writable-strings -Wno-mismatched-tags"
+    export CXXFLAGS="$CXXFLAGS -Wno-writable-strings -Wno-mismatched-tags -Wno-old-style-cast"
 fi
 
 make HEADER_TESTS=ON configure

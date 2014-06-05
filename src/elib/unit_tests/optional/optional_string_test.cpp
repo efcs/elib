5348b25bb8f6c4a2291015327d47f2f744590fb5
Eric Fiselier <eric@efcs.ca>
2014-06-04 22:02:02 -0600

    continue optional test refactor

--- src/elib/unit_tests/optional/optional_string_test.cpp
+++ src/elib/unit_tests/optional/optional_string_test.cpp
@@ -2,12 +2,12 @@
 #include <boost/test/unit_test.hpp>
 
 #include <elib/optional.hpp>
-using opt = elib::optional<int>;
+#include <string>
+using opt = elib::optional<std::string>;
 using elib::nullopt;
 using elib::in_place;
 
-/// Test the reference version of optional
-BOOST_AUTO_TEST_SUITE(elib_optional_int_test_suite)
+BOOST_AUTO_TEST_SUITE(elib_optional_optional_string_test_suite)
 
 BOOST_AUTO_TEST_CASE(default_ctor_test)
 {
@@ -40,7 +40,7 @@ BOOST_AUTO_TEST_CASE(uninit_copy_ctor_test)
 
 BOOST_AUTO_TEST_CASE(init_copy_ctor_test)
 {
-    int x = 1;
+    std::string const x = "hello";
     opt const t(x);
     opt t2(t);
     BOOST_REQUIRE(t.good() && t2.good());
@@ -57,22 +57,23 @@ BOOST_AUTO_TEST_CASE(uninit_move_ctor_test)
 
 BOOST_AUTO_TEST_CASE(init_move_ctor_test)
 {
-    int x = 1;
+    std::string const x = "hello";
     opt t(x);
     opt t2(static_cast<opt &&>(t));
     BOOST_REQUIRE(t2.good());
     BOOST_CHECK(t2.value() == x);
 }
 
+
 BOOST_AUTO_TEST_CASE(value_copy_ctor_test)
 {
     {
-        int x = 1;
+        std::string const x = "hello";
         opt t(x);
         BOOST_REQUIRE(t.good());
         BOOST_CHECK(t.value() == x);
     }{
-        int x = 1;
+        std::string const x = "hello";
         const opt t(x);
         BOOST_REQUIRE(t.good());
         BOOST_CHECK(t.value() == x);
@@ -81,32 +82,38 @@ BOOST_AUTO_TEST_CASE(value_copy_ctor_test)
 
 BOOST_AUTO_TEST_CASE(value_move_ctor_test)
 {
-    int const expect = 1;
+    std::string const expect = "hello";
     {
-        opt t(1);
+        opt t(std::string("hello"));
         BOOST_REQUIRE(t.good());
         BOOST_CHECK(t.value() == expect);
     }{
-        const opt t(1);
+        const opt t(std::string("hello"));
         BOOST_REQUIRE(t.good());
         BOOST_CHECK(t.value() == expect);
     }
 }
 
-
 BOOST_AUTO_TEST_CASE(in_place_ctor_test)
 {
-    int x = 1;
-    opt t(in_place, x);
+    std::string const expect = "hello";
+    opt t(in_place, "hello");
     BOOST_REQUIRE(t.good());
-    BOOST_CHECK(t.value() == x);
+    BOOST_CHECK(t.value() == expect);
 }
 
 BOOST_AUTO_TEST_CASE(in_place_empty_ctor_test)
 {
     opt t(in_place);
     BOOST_REQUIRE(t.good());
-    BOOST_CHECK(t.value() == 0);
+    BOOST_CHECK(t.value() == "");
+}
+
+BOOST_AUTO_TEST_CASE(initializer_list_in_place_ctor_test)
+{
+    const opt o1(in_place, {'H', 'e', 'l', 'l', 'o'});
+    BOOST_REQUIRE(o1.good());
+    BOOST_CHECK(o1.value() == "Hello");
 }
 
 BOOST_AUTO_TEST_CASE(uninit_nullopt_assign_test)
@@ -118,7 +125,7 @@ BOOST_AUTO_TEST_CASE(uninit_nullopt_assign_test)
 
 BOOST_AUTO_TEST_CASE(init_nullopt_assign_test)
 {
-    int x = 1;
+    std::string const x;
     opt t(x);
     t = nullopt;
     BOOST_CHECK(not t.good());
@@ -134,20 +141,20 @@ BOOST_AUTO_TEST_CASE(copy_assign_empty_to_empty_test)
 
 BOOST_AUTO_TEST_CASE(copy_assign_non_empty_to_empty_test)
 {
-    opt const t1(1);
+    opt const t1("hello");
     opt t2;
     t2 = t1;
     BOOST_REQUIRE(t1.good() && t2.good());
-    BOOST_CHECK(t2.value() == 1);
+    BOOST_CHECK(t2.value() == "hello");
 }
 
 BOOST_AUTO_TEST_CASE(copy_assign_non_empty_to_non_empty_test)
 {
-    opt const t1(1);
-    opt t2(0);
+    opt const t1("hello");
+    opt t2("world");
     t2 = t1;
     BOOST_REQUIRE(t2.good() && t1.good());
-    BOOST_CHECK(t2.value() == 1);
+    BOOST_CHECK(t2.value() == "hello");
 }
 
 
@@ -161,54 +168,61 @@ BOOST_AUTO_TEST_CASE(move_assign_empty_to_empty_test)
 
 BOOST_AUTO_TEST_CASE(move_assign_non_empty_to_empty_test)
 {
-    opt t1(1);
+    opt t1("hello");
     opt t2;
     t2 = static_cast<opt &&>(t1);
     BOOST_REQUIRE(t2.good());
-    BOOST_CHECK(t2.value() == 1);
+    BOOST_CHECK(t2.value() == "hello");
 }
 
 BOOST_AUTO_TEST_CASE(move_assign_non_empty_to_non_empty_test)
 {
-    opt t1(1);
-    opt t2(0);
+    opt t1("hello");
+    opt t2("bad");
     t2 = static_cast<opt &&>(t1);
     BOOST_REQUIRE(t2.good());
-    BOOST_CHECK(t2.value() == 1);
+    BOOST_CHECK(t2.value() == "hello");
 }
+
 
 BOOST_AUTO_TEST_CASE(assign_value_to_empty_test)
 {
     opt t;
-    t = 1;
+    t = std::string("hello");
     BOOST_REQUIRE(t.good());
-    BOOST_CHECK(t.value() == 1);
+    BOOST_CHECK(t.value() == "hello");
 }
 
 BOOST_AUTO_TEST_CASE(assign_value_to_non_empty_test)
 {
-    opt t(0);
-    t = 1;
+    opt t("bad");
+    t = std::string("hello");
     BOOST_REQUIRE(t.good());
-    BOOST_CHECK(t.value() == 1);
+    BOOST_CHECK(t.value() == "hello");
 }
-
 
 BOOST_AUTO_TEST_CASE(emplace_test)
 {
-    int x = 1;
     opt t;
-    t.emplace(x);
+    t.emplace("hello");
     BOOST_REQUIRE(t.good());
-    BOOST_CHECK(t.value() == x);
+    BOOST_CHECK(t.value() == "hello");
 }
 
 BOOST_AUTO_TEST_CASE(empty_emplace_test)
 {
-    opt t(1);
+    opt t("bad");
     t.emplace();
     BOOST_REQUIRE(t.good());
-    BOOST_CHECK(t.value() == 0);
+    BOOST_CHECK(t.value() == "");
+}
+
+BOOST_AUTO_TEST_CASE(initializer_list_emplace_test)
+{
+    opt t;
+    t.emplace({'h', 'e', 'l', 'l', 'o'});
+    BOOST_REQUIRE(t.good());
+    BOOST_CHECK(t.value() == "hello");
 }
 
 BOOST_AUTO_TEST_CASE(uninit_swap_test)
@@ -221,7 +235,7 @@ BOOST_AUTO_TEST_CASE(uninit_swap_test)
 
 BOOST_AUTO_TEST_CASE(one_init_swap_test)
 {
-    int x = 1;
+    std::string const x = "opt1";
     opt t1(x);
     opt t2;
     t1.swap(t2);
@@ -231,8 +245,8 @@ BOOST_AUTO_TEST_CASE(one_init_swap_test)
 
 BOOST_AUTO_TEST_CASE(both_init_swap_test)
 {
-    int x = 1;
-    int y = 1;
+    std::string const x = "opt1";
+    std::string const y = "opt2";
     opt t1(x);
     opt t2(y);
     t1.swap(t2);
@@ -243,7 +257,7 @@ BOOST_AUTO_TEST_CASE(both_init_swap_test)
 
 BOOST_AUTO_TEST_CASE(const_arrow_operator_test)
 {
-    int x = 1;
+    std::string const x = "hello";
     opt const t(x);
     BOOST_REQUIRE(t.operator->());
     BOOST_CHECK(*(t.operator->()) == x);
@@ -251,37 +265,36 @@ BOOST_AUTO_TEST_CASE(const_arrow_operator_test)
 
 BOOST_AUTO_TEST_CASE(non_const_arrow_operator_test)
 {
-    int x = 1;
+    std::string const x = "hello";
     opt t(x);
     BOOST_REQUIRE(t.operator->());
     BOOST_CHECK(*(t.operator->()) == x);
-    *(t.operator->()) = 2;
-    BOOST_CHECK(t.value() == 2);
+    *(t.operator->()) = "world";
+    BOOST_CHECK(t.value() == "world");
 }
 
 BOOST_AUTO_TEST_CASE(const_deref_operator_test)
 {
-    int x = 1;
+    std::string const x = "hello";
     opt const t(x);
     BOOST_CHECK(t.operator*() == x);
 }
 
 BOOST_AUTO_TEST_CASE(non_const_lvalue_deref_operator_test)
 {
-    int x = 1;
+    std::string const x = "hello";
     opt t(x);
     BOOST_CHECK(t.operator*() == x);
-    t.operator*() = 2;
-    BOOST_CHECK(t.operator*() == 2);
+    t.operator*() = "world";
+    BOOST_CHECK(t.operator*() == "world");
 }
 
 BOOST_AUTO_TEST_CASE(rvalue_deref_operator_test)
 {
-    int x = 1;
+    std::string const x = "hello";
     opt t(x);
     BOOST_CHECK(static_cast<opt &&>(t).operator*() == x);
 }
-
 
 BOOST_AUTO_TEST_CASE(empty_operator_bool_test)
 {
@@ -291,8 +304,7 @@ BOOST_AUTO_TEST_CASE(empty_operator_bool_test)
 
 BOOST_AUTO_TEST_CASE(non_empty_operator_bool_test)
 {
-    int x = 1;
-    opt const t(x);
+    opt const t("test");
     BOOST_CHECK(bool(t));
 }
 
@@ -304,15 +316,14 @@ BOOST_AUTO_TEST_CASE(empty_good_test)
 
 BOOST_AUTO_TEST_CASE(non_empty_good_test)
 {
-    int x = 1;
-    opt const t(x);
+    opt const t("test");
     BOOST_CHECK(t.good());
 }
 
 BOOST_AUTO_TEST_CASE(const_value_test)
 {
-    opt const t(1);
-    BOOST_CHECK(t.value() == 1);
+    opt const t("hello");
+    BOOST_CHECK(t.value() == "hello");
 }
 
 BOOST_AUTO_TEST_CASE(empty_const_value_test)
@@ -323,10 +334,10 @@ BOOST_AUTO_TEST_CASE(empty_const_value_test)
 
 BOOST_AUTO_TEST_CASE(non_const_value_test)
 {
-    opt t(1);
-    BOOST_CHECK(t.value() == 1);
-    t.value() = 2;
-    BOOST_CHECK(t.value() == 2);
+    opt t("hello");
+    BOOST_CHECK(t.value() == "hello");
+    t.value() = "world";
+    BOOST_CHECK(t.value() == "world");
 }
 
 BOOST_AUTO_TEST_CASE(empty_non_const_value_test)
@@ -337,8 +348,8 @@ BOOST_AUTO_TEST_CASE(empty_non_const_value_test)
 
 BOOST_AUTO_TEST_CASE(rvalue_value_test)
 {
-    opt t(1);
-    BOOST_CHECK(static_cast<opt &&>(t).value() == 1);
+    opt t("hello");
+    BOOST_CHECK(static_cast<opt &&>(t).value() == "hello");
 }
 
 BOOST_AUTO_TEST_CASE(empty_rvalue_value_test)
@@ -349,26 +360,26 @@ BOOST_AUTO_TEST_CASE(empty_rvalue_value_test)
 
 BOOST_AUTO_TEST_CASE(const_value_or_test)
 {
-    opt const t(1);
-    BOOST_CHECK(t.value_or(2) == 1);
+    opt const t("hello");
+    BOOST_CHECK(t.value_or("world") == "hello");
 }
 
 BOOST_AUTO_TEST_CASE(empty_const_value_or_test)
 {
     opt const t;
-    BOOST_CHECK(t.value_or(2) == 2);
+    BOOST_CHECK(t.value_or("world") == "world");
 }
 
 BOOST_AUTO_TEST_CASE(rvalue_value_or_test)
 {
-    opt t(1);
-    BOOST_CHECK(static_cast<opt &&>(t).value_or(2) == 1);
+    opt t("hello");
+    BOOST_CHECK(static_cast<opt &&>(t).value_or("world") == "hello");
 }
 
 BOOST_AUTO_TEST_CASE(empty_rvalue_value_or_test)
 {
     opt t;
-    BOOST_CHECK(static_cast<opt &&>(t).value_or(2) == 2);
+    BOOST_CHECK(static_cast<opt &&>(t).value_or("world") == "world");
 }
 
 BOOST_AUTO_TEST_SUITE_END()

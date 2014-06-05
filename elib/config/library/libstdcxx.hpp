f5d9a45bfed304255ed292b2833b028f40956959
Eric Fiselier <eric@efcs.ca>
2014-06-05 02:18:39 -0600

    changed no ref qualifiers workaround

--- elib/config/library/libstdcxx.hpp
+++ elib/config/library/libstdcxx.hpp
@@ -21,4 +21,10 @@
 #   define ELIB_CONFIG_HAS_REGEX 1
 # endif
 # 
+# /** GCC claims they support ref qualifiers in 4.8.1
+ * But they are buggy and cause compile errors */
+# if __GNUC__ < 4 || (__GNUC__ == 4 && __GNUC_MINOR__ <= 8) 
+#   define ELIB_CONFIG_NO_REF_QUALIFIERS 1
+# endif
+# 
 #endif /* ELIB_CONFIG_LIBRARY_LIBSTDCXX_HPP */

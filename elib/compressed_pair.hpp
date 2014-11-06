3636b127382c0bc3a79ea294fad2bef20fc489d4
Eric Fiselier <eric@efcs.ca>
2014-11-05 20:28:42 -0600

    whitespace fix

--- elib/compressed_pair.hpp
+++ elib/compressed_pair.hpp
@@ -14,44 +14,44 @@
 
 namespace elib 
 {
-    
+
     ////////////////////////////////////////////////////////////////////////////
     enum class compressed_pair_type
     {
         none, first, second, both
     };
-        
+
     namespace compressed_pair_detail
     {
         ////////////////////////////////////////////////////////////////////////
         template <bool IsSame, bool FirstEmpty, bool SecondEmpty>
         struct choose_compress_impl;
-        
+
         template <bool IsSame>
         struct choose_compress_impl<IsSame, false, false>
           : elib::integral_constant<compressed_pair_type, compressed_pair_type::none>
         {};
-        
+
         template <bool IsSame>
         struct choose_compress_impl<IsSame, true, false>
           : elib::integral_constant<compressed_pair_type, compressed_pair_type::first>
         {};
-        
+
         template <bool IsSame>
         struct choose_compress_impl<IsSame, false, true>
           : elib::integral_constant<compressed_pair_type, compressed_pair_type::second>
         {};
-        
+
         template <>
         struct choose_compress_impl<false, true, true>
           : elib::integral_constant<compressed_pair_type, compressed_pair_type::both>
         {};
-        
+
         template <>
         struct choose_compress_impl<true, true, true>
           : elib::integral_constant<compressed_pair_type, compressed_pair_type::first>
         {};
-        
+
 # if defined(ELIB_CONFIG_CLANG)
 #   if __has_feature(is_final)
 #     define ELIB_COMPRESSED_PAIR_HAS_FINAL
@@ -69,16 +69,16 @@ namespace elib
           , aux::is_empty<Second>::value && !__is_final(Second)
 # endif
           >;
-        
+
 # undef ELIB_COMPRESSED_PAIR_HAS_FINAL
-        
+
         ////////////////////////////////////////////////////////////////////////
         template <
             class First, class Second
           , compressed_pair_type = choose_compress<First, Second>::value
           >
         class compressed_pair_impl;
-        
+
         ////////////////////////////////////////////////////////////////////////
         template <class First, class Second>
         class compressed_pair_impl<First, Second, compressed_pair_type::none>
@@ -86,32 +86,32 @@ namespace elib
         public:
             using first_type = First;
             using second_type = Second;
-            
+
             using first_reference = aux::remove_reference_t<First> &;
             using second_reference = aux::remove_reference_t<Second> &;
-            
+
             using first_const_reference = aux::remove_reference_t<First> const &;
             using second_const_reference = aux::remove_reference_t<Second> const &;
-            
+
             constexpr compressed_pair_impl()
               : m_first(), m_second()
             {}
-            
+
             constexpr compressed_pair_impl(First f, Second s)
               : m_first(elib::forward<First>(f))
               , m_second(elib::forward<Second>(s))
             {}
-            
+
             first_reference first() noexcept { return m_first; }
-            
-            constexpr first_const_reference 
+
+            constexpr first_const_reference
             first() const noexcept { return m_first; }
-            
+
             second_reference second() noexcept { return m_second; }
-            
-            constexpr second_const_reference 
+
+            constexpr second_const_reference
             second() const noexcept { return m_second; }
-            
+
             void swap(compressed_pair_impl & other)
                 noexcept(aux::is_nothrow_swappable<First>::value 
                         && aux::is_nothrow_swappable<Second>::value)
@@ -125,8 +125,8 @@ namespace elib
             First m_first;
             Second m_second;
         };
-        
-        
+
+
         ////////////////////////////////////////////////////////////////////////
         template <class First, class Second>
         class compressed_pair_impl<First, Second, compressed_pair_type::first>
@@ -135,32 +135,32 @@ namespace elib
         public:
             using first_type = First;
             using second_type = Second;
-            
+
             using first_reference = First &;
             using second_reference = aux::remove_reference_t<Second> &;
-            
+
             using first_const_reference = First const &;
             using second_const_reference = aux::remove_reference_t<Second> const &;
-            
+
             constexpr compressed_pair_impl() 
               : First(), m_second()
             {}
-            
+
             constexpr compressed_pair_impl(First f, Second s)
               : First(elib::forward<First>(f))
               , m_second(elib::forward<Second>(s))
             {}
-            
+
             first_reference first() noexcept { return *this; }
-            
+
             constexpr first_const_reference 
             first() const noexcept { return *this; }
-            
+
             second_reference second() noexcept { return m_second; }
-            
+
             constexpr second_const_reference 
             second() const noexcept { return m_second; }
-            
+
             void swap(compressed_pair_impl & other)
                 noexcept(aux::is_nothrow_swappable<First>::value 
                         && aux::is_nothrow_swappable<Second>::value)
@@ -168,12 +168,12 @@ namespace elib
                 using std::swap;
                 swap(m_second, other.m_second);
             }
-            
+
         private:
             Second m_second;
         };
-        
-        
+
+
         ////////////////////////////////////////////////////////////////////////
         template <class First, class Second>
         class compressed_pair_impl<First, Second, compressed_pair_type::second>
@@ -182,32 +182,32 @@ namespace elib
         public:
             using first_type = First;
             using second_type = Second;
-            
+
             using first_reference = aux::remove_reference_t<First> &;
             using second_reference = Second &;
-            
+
             using first_const_reference = aux::remove_reference_t<First> const &;
             using second_const_reference = Second const &;
-            
+
             constexpr compressed_pair_impl()
               : Second(), m_first()
             {}
-            
+
             constexpr compressed_pair_impl(First f, Second s)
               : Second(elib::forward<Second>(s))
               , m_first(elib::forward<First>(f))
             {}
-            
+
             first_reference first() noexcept { return m_first; }
-            
+
             constexpr first_const_reference 
             first() const noexcept { return m_first; }
-            
+
             second_reference second() noexcept { return *this; }
-            
+
             constexpr second_const_reference 
             second() const noexcept { return *this; }
-            
+
             void swap(compressed_pair_impl & other)
                 noexcept(aux::is_nothrow_swappable<First>::value 
                         && aux::is_nothrow_swappable<Second>::value)
@@ -215,12 +215,12 @@ namespace elib
                 using std::swap;
                 swap(m_first, other.m_first);
             }
-            
-            
+
         private:
             First m_first;
         };
-        
+
+
         ////////////////////////////////////////////////////////////////////////
         template <class First, class Second>
         class compressed_pair_impl<First, Second, compressed_pair_type::both>
@@ -229,34 +229,34 @@ namespace elib
         public:
             using first_type = First;
             using second_type = Second;
-            
+
             using first_reference = First &;
             using second_reference = Second &;
-            
+
             using first_const_reference = First const &;
             using second_const_reference = Second const &;
-            
+
         public:
-            
+
             constexpr compressed_pair_impl()
               : First(), Second() 
             {}
-            
+
             constexpr compressed_pair_impl(First f, Second s)
               : First(elib::forward<First>(f))
               , Second(elib::forward<Second>(s))
             {}
-            
+
             first_reference first() noexcept { return *this; }
-            
+
             constexpr first_const_reference
             first() const noexcept { return *this; }
-            
+
             second_reference second() noexcept { return *this; }
-            
+
             constexpr second_const_reference 
             second() const noexcept { return *this; }
-            
+
             void swap(compressed_pair_impl &)
                 noexcept(aux::is_nothrow_swappable<First>::value 
                         && aux::is_nothrow_swappable<Second>::value)
@@ -264,7 +264,7 @@ namespace elib
             }
         };
     }                                       // namespace compressed_pair_detail
-    
+
     ////////////////////////////////////////////////////////////////////////////
     template <class First, class Second>
     class compressed_pair
@@ -279,7 +279,7 @@ namespace elib
         using second_reference = typename base::second_reference;
         using first_const_reference = typename base::first_const_reference;
         using second_const_reference = typename base::second_const_reference;
-        
+
         static constexpr compressed_pair_type compression = 
             compressed_pair_detail::choose_compress<First, Second>::value;
 
@@ -287,21 +287,21 @@ namespace elib
         constexpr compressed_pair()
           : base()
         {}
-        
+
         constexpr compressed_pair(first_type f, second_type s)
           : base(elib::forward<first_type>(f), elib::forward<second_type>(s))
         {}
-        
+
         first_reference first() noexcept { return base::first(); }
-        
+
         constexpr first_const_reference 
         first() const noexcept { return base::first(); }
-        
+
         second_reference second() noexcept { return base::second(); }
-        
+
         constexpr second_const_reference 
         second() const noexcept { return base::second(); }
-        
+
         void swap(compressed_pair & other)
             noexcept( aux::is_nothrow_swappable<First>::value
                      && aux::is_nothrow_swappable<Second>::value)
@@ -309,12 +309,13 @@ namespace elib
             base::swap(other);
         }
     };
-    
+
+
     ////////////////////////////////////////////////////////////////////////////
     template <class First, class Second>
     constexpr compressed_pair_type 
     compressed_pair<First, Second>::compression;
-    
+
     ////////////////////////////////////////////////////////////////////////////
     template <class First, class Second>
     inline void swap(
@@ -325,7 +326,7 @@ namespace elib
     {
         lhs.swap(rhs);
     }
-    
+
     ////////////////////////////////////////////////////////////////////////////
     template <class First, class Second>
     constexpr bool operator==(
@@ -335,7 +336,7 @@ namespace elib
     {
         return (lhs.first() == rhs.first() && lhs.second() == rhs.second());
     }
-    
+
     ////////////////////////////////////////////////////////////////////////////
     template <class First, class Second>
     constexpr bool operator!=(
@@ -345,7 +346,7 @@ namespace elib
     {
         return (lhs.first() != rhs.first() || lhs.second() != rhs.second());
     }
-    
+
     ////////////////////////////////////////////////////////////////////////////
     template <class First, class Second>
     constexpr bool operator<(
@@ -356,7 +357,7 @@ namespace elib
         return lhs.first() < rhs.first() ? true :
             (rhs.first() < lhs.first() ? false : lhs.second() < rhs.second());
     }
-    
+
     ////////////////////////////////////////////////////////////////////////////
     template <class First, class Second>
     constexpr bool operator<=(
@@ -366,7 +367,7 @@ namespace elib
     {
         return !(rhs < lhs);
     }
-    
+
     ////////////////////////////////////////////////////////////////////////////
     template <class First, class Second>
     constexpr bool operator>(
@@ -376,7 +377,7 @@ namespace elib
     {
         return rhs < lhs;
     }
-    
+
     ////////////////////////////////////////////////////////////////////////////
     template <class First, class Second>
     constexpr bool operator>=(
@@ -386,7 +387,7 @@ namespace elib
     {
         return !(lhs < rhs);
     }
-    
+
     ////////////////////////////////////////////////////////////////////////////
     template <class First, class Second>
     inline compressed_pair<aux::decay_t<First>, aux::decay_t<Second>>
@@ -396,6 +397,6 @@ namespace elib
             elib::forward<First>(first), elib::forward<Second>(second)
           );
     }
-    
+
 }                                                           // namespace elib
 #endif /* ELIB_COMPRESSED_PAIR_HPP */
